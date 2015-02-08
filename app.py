f1f2c9a2d39c547def4466948bbc623301ae8029
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2015-02-08 13:54:50 +0100

    Templates, js+css (bootstrap) + SocketIO

--- app.py
+++ app.py
@@ -1,25 +1,87 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+import gevent
+from flask.ext.socketio import SocketIO, emit
 import pygal
+from flask_wtf import Form
+from wtforms import StringField
+import random
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+    return app
+
+app = create_app()
+app.config['SECRET_KEY'] = 'secret123'
 app.debug = True
+socketio = SocketIO(app)
+
+class MyForm(Form):
+    emit_name = StringField('emit_name')
+    emit_broadcast = StringField('emit_broadcast')
 
 
 @app.route('/')
 def root():
-    return "Hello World"
+    return render_template('index.html')
+
+
+@app.route('/testws')
+def testws():
+    return render_template('testws.html', form=MyForm())
+
+
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']})
+
+
+@socketio.on('my broadcast event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
+
+
+@socketio.on('random_nr', namespace='/test')
+def gen_random_nr():
+    while True:
+        emit(
+            'crap_data',
+            {'data': random.randint(0, 1000000)},
+            namespace='/test')
+        gevent.sleep(1)
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('connected')
+
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print("Client disconnected")
 
 
 @app.route('/line_chart')
 def line_chart():
-    line_chart = pygal.StackedLine(fill=True, 
-            interpolate='cubic', 
-            style=pygal.style.LightColorizedStyle)
+    line_chart = pygal.StackedLine(
+        fill=True,
+        interpolate='cubic',
+        style=pygal.style.LightColorizedStyle)
     line_chart.title = 'Browser usage evolution (in %)'
     line_chart.x_labels = map(str, range(2002, 2013))
-    line_chart.add('Firefox', [None, None, 0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
-    line_chart.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
-    line_chart.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-    line_chart.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
+    line_chart.add(
+        'Firefox', [None, None, 0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
+    line_chart.add(
+        'Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
+    line_chart.add(
+        'IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
+    line_chart.add(
+        'Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
     chart = line_chart.render(is_unicode=True)
-    return render_template('base.html', chart=chart)
+    return render_template('chart.html', chart=chart)
+
+
+if __name__ == "__main__":
+    socketio.run(app)
---
+++ static/testws.js
@@ -0,0 +1,51 @@
+var socket;
+$(document).ready(function(){
+    socket = io.connect('http://localhost:5000/test');
+    socket.on('my response', function(msg) {
+        $('#log').append('<p>Received: ' + msg.data + '</p>');
+    });
+
+    socket.on('crap_data', function(msg) {
+        $('#log').append("<p>Data: " + msg.data + "</p>");
+    });
+
+    socket.on('connected', function() {
+        socket.emit('random_nr');
+    });
+
+    $('form#emit').submit(function(event) {
+        console.log("emit..");
+        socket.emit('my event', {data: $('#emit_name').val()});
+        return false;
+    });
+
+    $('form#broadcast').submit(function(event) {
+        console.log("broadcast..");
+        socket.emit('my broadcast event', {data: $('#emit_broadcast').val()});
+        return false;
+    });
+
+
+    $('form').submit(function(event) {
+        var data = $("#emit_name").val();
+        var broadcast = $("#emit_broadcast").val();
+
+        if (data && !broadcast) {
+            console.log("my event: ", data);
+            socket.emit('my event', {data: data});
+        } else if (!data && broadcast) {
+            console.log("my broadcast event: ", broadcast);
+            socket.emit('my broadcast event', {data: broadcast});
+        } else if (data && broadcast) {
+            socket.emit('my event', {data: data});
+            socket.emit('my broadcast event', {data: broadcast});
+        } else {
+            console.log("data: ", data);
+            console.log("broadcast: ", broadcast);
+            $('#log').append('<p>Missing data..</p>');
+        }
+
+        //socket.emit('my broadcast event', {data: $('#emit_data').val()});
+        return false;
+    });
+});
