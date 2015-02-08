85606b75f5a98c754ce3b6a07b4cb76b1a3703af
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2015-02-08 14:07:34 +0100

    fixed path to backend service by javascript

--- static/testws.js
+++ static/testws.js
@@ -1,6 +1,6 @@
 var socket;
 $(document).ready(function(){
-    socket = io.connect('http://localhost:5000/test');
+    socket = io.connect(document.location.protocol'://' + document.location.hostname + ':' + document.location.port + '/' + document.location.pathname);
     socket.on('my response', function(msg) {
         $('#log').append('<p>Received: ' + msg.data + '</p>');
     });
